ee2a638cdf9d776ecc49e27514f65d26438914ea
Nishikag02 <nishikagupta0@gmail.com>
2025-11-12 11:35:45 +0530

    added assignment files

---
+++ Assignment_2/q4c.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char s[100];
+    cout << "Enter string: ";
+    cin.getline(s, 100);
+    int j = 0;
+    for (int i = 0; s[i] != '\0'; i++) {
+        char c = s[i];
+        if (!(c=='a'||c=='e'||c=='i'||c=='o'||c=='u'||c=='A'||c=='E'||c=='I'||c=='O'||c=='U'))
+            s[j++] = s[i];
+    }
+    s[j] = '\0';
+    cout << "Without vowels: " << s << endl;
+    return 0;
+}
